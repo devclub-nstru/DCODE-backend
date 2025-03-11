029ac6a58d75236f24cea42bfd8f01863db20e3a
Raghavg18 <graghav2006@gmail.com>
2025-03-11 23:14:10 +0530

    init

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,5 @@
 import User from '../models/usermodel.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
